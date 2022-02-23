acf43dd0c3fb6375626aa80a95fd962953e07408
@Ash9311 <ashutoshmulki7@gmail.com>
2022-02-24 00:16:06 +0530

    mongoose connection

--- app.js
+++ app.js
@@ -11,13 +11,36 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongoDB://localhost:27017/todolistDB",{useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser: true});
 
 const itemsSchema = {
   name:String
-}
+};
 
 const Item = mongoose.model("Item",itemsSchema)
+
+const Item1 = new Item({
+  name:"Welcome to your todoList!"
+});
+
+const Item2 = new Item({
+  name: "Hit the + button to add new item."
+});
+
+const Item3 = new Item({
+  name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [Item1,Item2,Item3];
+
+Item.insertMany(defaultItems,function(err){
+  if(err){
+    console.log(err);
+  }
+  else{
+    console.log("Successfully saved default items to DB.");
+  }
+})
 
 app.get("/", function(req, res) {
 
